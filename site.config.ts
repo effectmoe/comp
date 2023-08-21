9e32e9bb39d0b70cc6b6f9c10eccc646000dec18
EFFECT <info@effect.moe>
2023-08-21 11:55:23 +0900

    8.21

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5e57dffdc45044b6b9a206c31467bc2c',
+  rootNotionPageId: '1f8f56d5375d4d5b959e364119ebcef0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
